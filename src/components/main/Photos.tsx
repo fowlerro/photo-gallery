1f4f1a77dca101c7bf8903dbae20680cb14955f0
fowlerro <fwlerro@gmail.com>
2022-06-26 01:15:42 +0200

    add photo delete button

--- src/components/main/Photos.tsx
+++ src/components/main/Photos.tsx
@@ -2,19 +2,25 @@ import React from 'react';
 
 import { useLiveQuery } from 'dexie-react-hooks';
 
-import { List } from 'antd';
+import { Button, List } from 'antd';
+import { DeleteFilled } from '@ant-design/icons';
 
-import { db } from '../../utils/db';
+import { db, Photo } from '../../utils/db';
 
 export default function Photos(): JSX.Element {
 	const photos = useLiveQuery(() => db.photos.toArray());
+
+	const handleDelete = (id: Photo['id']) => {
+		if (!id) return;
+		db.photos.delete(id);
+	};
 
 	return (
 		<List
 			itemLayout='horizontal'
 			dataSource={photos}
 			renderItem={item => (
-				<List.Item>
+				<List.Item actions={[<Button icon={<DeleteFilled />} key='delete' onClick={() => handleDelete(item.id)} />]}>
 					<List.Item.Meta
 						avatar={<img src={item.photo} alt={item.fileName} />}
 						title={item.title}
